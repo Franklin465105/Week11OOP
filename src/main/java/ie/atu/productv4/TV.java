1500f4ac8ee1ef01321a52fc2344316abf799be5
Franklin Esedebe <G00465105@atu.ie>
2025-12-03 17:05:07 +0000

    Set up TV Class

---
+++ src/main/java/ie/atu/productv4/TV.java
@@ -0,0 +1,38 @@
+package ie.atu.productv4;
+
+import ie.atu.productv3.Product;
+
+
+public class TV extends Product {
+
+    private String manufacturer;
+    private int size;
+
+    public TV() {
+        super();
+        manufacturer = "";
+        size = 0;
+        count++;
+    }
+
+   public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+   }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " by " + manufacturer + "\nThe Size is" + size + "'";
+    }
+}
