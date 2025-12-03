2161ee4c1828fafe4a6e8a97b15a13e4663098ae
Franklin Esedebe <G00465105@atu.ie>
2025-12-03 17:28:42 +0000

    Finished Task 3

--- src/main/java/ie/atu/productv4/ProductDB.java
+++ src/main/java/ie/atu/productv4/ProductDB.java
@@ -11,6 +11,8 @@ public class ProductDB {
         Book myBook = null;
         Software mySoftware = null;
         Product myProduct = null;
+        Music myMusic = null;
+        TV myTV = null;
         if (productCode.equalsIgnoreCase("java")) {
             myBook = new Book();
             myBook.setCode(productCode);
@@ -55,6 +57,56 @@ public class ProductDB {
             mySoftware.setPrice(54.50);
             mySoftware.setVersion("Oracle 3.0");
             myProduct = mySoftware;
+        }
+        if (productCode.equalsIgnoreCase("Pink")) {
+            myMusic = new Music();
+            myMusic.setCode(productCode);
+            myMusic.setDescription("Wish you were here by ");
+            myMusic.setPrice(8);
+            myMusic.setArtist("Pink Floyd");
+            myMusic.setLabel("Part of the Columbia group");
+            myProduct = myMusic;
+        } else if (productCode.equalsIgnoreCase("J456")) {
+            myMusic = new Music();
+            myMusic.setCode(productCode);
+            myMusic.setDescription("Poison");
+            myMusic.setPrice(1);
+            myMusic.setArtist("Jaydes");
+            myMusic.setLabel("Galactic Songs");
+            myProduct = myMusic;
+        }else if (productCode.equalsIgnoreCase("DD5")) {
+            myMusic = new Music();
+            myMusic.setCode(productCode);
+            myMusic.setDescription("Headlines");
+            myMusic.setPrice(3);
+            myMusic.setArtist("Drake");
+            myMusic.setLabel("OVO");
+            myProduct = myMusic;
+        }
+        if (productCode.equalsIgnoreCase("kdl43")) {
+            myTV = new TV();
+            myTV.setCode(productCode);
+            myTV.setDescription("SONY BRAVIA SMART TV KDL43WF663");
+            myTV.setPrice(819);
+            myTV.setManufacturer("Sony");
+            myTV.setSize(55);
+            myProduct = myTV;
+        } else if (productCode.equalsIgnoreCase("AP4")) {
+            myTV = new TV();
+            myTV.setCode(productCode);
+            myTV.setDescription("Flatscreen TV");
+            myTV.setPrice(999.99);
+            myTV.setManufacturer("Apple");
+            myTV.setSize(30);
+            myProduct = myTV;
+        } else if (productCode.equalsIgnoreCase("MM5")) {
+            myTV = new TV();
+            myTV.setCode(productCode);
+            myTV.setDescription("Microsoft TV");
+            myTV.setPrice(852);
+            myTV.setManufacturer("Microsoft");
+            myTV.setSize(31);
+            myProduct = myTV;
         }
 
         return myProduct;
